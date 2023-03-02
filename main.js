0a930ba69ee1cd847005affe3c0051e74ba15182
JSDG2802 <102630801+JSDG2802@users.noreply.github.com>
2023-03-02 17:53:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var objects = [];
+  var objects = [];
 var objectDetector = "";
 var img = "";
 var estatus = "";
@@ -6,7 +6,11 @@ function setup()
 {
   canvas = createCanvas(640, 420);
   canvas.center();
+  video = createCapture(VIDEO);
+  video.size(640, 420);
+  video.hide();
   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("estatus").innerHTML = "Estado: detectando objetos";
 }
 function preload() 
 {
@@ -17,8 +21,25 @@ function preload()
 }
 function draw() 
 {
-  background("white");
-  image(img1,0,0,640,420);
+  background("green");
+  //image(img1,0,0,640,420);
+  image(video, 0, 0, 380, 380);
+      if(estatus != "")
+      {     
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) 
+        {
+          //document.getElementById("estatus").innerHTML = "Estado: objeto detectado";
+          //document.getElementById("number_of_objects").innerHTML = "El número de objetos detectados es: "+ objects.length;
+ 
+          fill("blue");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("cyan");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
 }
 function modelLoaded()
 {
@@ -35,5 +56,6 @@ function gotResults(error,results)
    else
    {
      console.log(results);
+     objects = results;
    }
 }
